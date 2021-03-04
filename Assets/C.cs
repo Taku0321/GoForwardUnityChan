6d68df57b685d15be432f6fe0323b20f5016d778
Taku0321 <idea.katachi@gmail.com>
2021-03-04 18:31:26 +0900

    Initial commit

---
+++ Assets/C.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C : MonoBehaviour
+{
+
+    public AudioClip block;
+    AudioSource audioSource;
+
+    private GameObject ground;
+    private string Touch = "TouchTag";
+
+    void Start()
+    {
+        //Componentを取得
+        audioSource = GetComponent<AudioSource>();
+
+        this.ground = GameObject.Find("ground");
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == Touch)
+        {
+            audioSource.PlayOneShot(block);
+        }
+    }
+}
